2ecfb8ab03584396225191b726aa7d991056a08c
David Stonely <david.stonely@oxos.com>
2024-04-02 16:00:45 -0400

    add

---
+++ uthsim.py
@@ -0,0 +1,20 @@
+
+
+
+class Uthsim:
+    def __init__(self, stack, ante):
+        self.stack = stack
+        self.ante = ante
+        self.totalGain = 0
+        self.totalBet = 0
+        self.maxStack = 0
+        self.wins = 0
+        self.losses = 0
+        self.pushes = 0
+        self.winstreak = 0
+        self.losestreak = 0
+        self.maxwinstreak = 0
+        self.maxlosestreak = 0
+
+
+
